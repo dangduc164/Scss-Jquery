a6ebfc9f5d7f5304981692d682ff8dc16cd76ae3
DangDuc <ducden164@gmail.com>
2021-12-10 10:27:00 +0700

    Signed-off-by: DangDuc <ducden164@gmail.com>

--- Jquery/js/home.js
+++ Jquery/js/home.js
@@ -4,10 +4,10 @@ $(function() {
 $("#send").on("click", function() {
     alert("bạn đã gửi thành công!");
 });
-$(".text-menu").on("click", function(e) {
-    $("body").toggleClass("open");
-    e.preventDefault();
-});
-$(document).ready(function() {
-    $('.header').show;
-});
+// $(".text-menu").on("click", function(e) {
+//     $("body").toggleClass("open");
+//     e.preventDefault();
+// });
+// $(document).ready(function() {
+//     $('.header').show;
+// });
